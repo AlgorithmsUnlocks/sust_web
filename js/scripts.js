6ffb9c97c8a796f86bd76dbe9ca80f398065ea6b
Ruman Ahmed <108632511+AlgorithmsUnlocks@users.noreply.github.com>
2024-06-30 00:47:17 +0600

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function toggleSubmenu() {
+    const submenu = document.querySelector('.sub-menu');
+    submenu.style.display = submenu.style.display === 'none'? 'block' : 'none';
+}
+
+function showImage() {
+    document.getElementById('img-course').classList.remove('d-none');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+});
